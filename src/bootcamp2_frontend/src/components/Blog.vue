6d182039cb29b3d8169c61844ae364fe64536d2f
LackerSKY <lackersky@gmail.com>
2024-06-25 19:52:45 +0200

    added way to add new wpis

--- src/bootcamp2_frontend/src/components/Blog.vue
+++ src/bootcamp2_frontend/src/components/Blog.vue
@@ -3,6 +3,8 @@
         <button @click="pobierzWpisy">refresh</button>
         siema blog!
         {{ wpisy }}
+        <input v-model="nowyBlog" type="text">
+        <button @click="dodajWpisy">dodaj</button>
     </div>
 </template>
 
@@ -12,10 +14,14 @@ import { bootcamp2_backend } from 'declarations/bootcamp2_backend/index';
 export default {
     data() {
         return {
-            wpisy: []
+            wpisy: [],
+            nowyBlog: ""
         }
     },
     methods: {
+        async dodajWpisy() {
+            await bootcamp2_backend.dodaj_wpis(this.nowyBlog);
+        },
         async pobierzWpisy() {
             this.wpisy = await bootcamp2_backend.odczytaj_wpisy();
         }
