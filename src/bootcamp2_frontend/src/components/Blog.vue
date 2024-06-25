b78150cf8fb8bb3fe8ee94bca4924c81adc835f9
LackerSKY <lackersky@gmail.com>
2024-06-25 19:39:33 +0200

    added way to list wpisy

--- src/bootcamp2_frontend/src/components/Blog.vue
+++ src/bootcamp2_frontend/src/components/Blog.vue
@@ -1,5 +1,24 @@
 <template>
     <div>
+        <button @click="pobierzWpisy">refresh</button>
         siema blog!
+        {{ wpisy }}
     </div>
 </template>
+
+<script>
+import { bootcamp2_backend } from 'declarations/bootcamp2_backend/index';
+
+export default {
+    data() {
+        return {
+            wpisy: []
+        }
+    },
+    methods: {
+        async pobierzWpisy() {
+            this.wpisy = await bootcamp2_backend.odczytaj_wpisy();
+        }
+    },
+}
+</script>
